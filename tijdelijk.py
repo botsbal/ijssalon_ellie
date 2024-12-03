45d0978bc5c05c40d6a3d5ed19d5af778e2481ec
“botsbal” <botsbal@hotmail.com>
2024-12-03 17:29:24 +0100

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,32 @@
+#dictionary prijzen
+prijzen = {
+    "aardbei": 3,
+    "Vanille" : 4,
+    "Chocolade" : 5,
+}
+
+#variabele aanbieding
+aanbieding = prijzen["aardbei"] * 0.8
+#print(aanbieding)
+
+#variabele reclame_tekst
+reclame_tekst = f"Vandaag in de aanbieding: aardbei-ijs, 1 liter – slechts € {aanbieding}"
+reclame_tekst2 = reclame_tekst[:62]
+
+#print(reclame_tekst)
+#print(reclame_tekst2)
+
+#tekst in hoofdletters
+reclame_tekst3 = (reclame_tekst2.upper())
+#print(reclame_tekst3)
+
+#voor drukker
+reclame_tekst4 = reclame_tekst3.split()
+#print(reclame_tekst4)
+
+#voor flyer
+for el in reclame_tekst4:
+    if len(el) >= 5:
+        print(el.upper()) #woorden met vijf of meer letters in hoofdletters
+    else:
+        print(el.lower()) #woorden met 4 of minder karakters in kleine letters
